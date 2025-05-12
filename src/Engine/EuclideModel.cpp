25c56c4f66c93f205ebad2ce3f961620474a5cfb
AnthonySTZ <anthonycandy2309@gmail.com>
2025-05-13 01:27:12 +0200

    remove second vbo for vao wirefram

--- src/Engine/EuclideModel.cpp
+++ src/Engine/EuclideModel.cpp
@@ -44,7 +44,7 @@ void EuclideModel::initBuffers()
 
 	// Bind and upload index data
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, faceIndicesBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.triangulateIndices.size() * sizeof(unsigned int), mesh.triangulateIndices.data(), GL_DYNAMIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.triangulateIndices.size() * sizeof(uint32_t), mesh.triangulateIndices.data(), GL_DYNAMIC_DRAW);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -52,27 +52,14 @@ void EuclideModel::initBuffers()
 
 
 	/* ------------ VAO WIREFRAME -------------- */
-
-
 	glGenVertexArrays(1, &vaoWireframe);
 	glGenBuffers(1, &wireframeIndicesBuffer);
 
 	glBindVertexArray(vaoWireframe);
 
-	// Bind and upload vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size() * sizeof(Vertex), mesh.vertices.data(), GL_DYNAMIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
-	glEnableVertexAttribArray(2);
-
 	// Bind and upload index data
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, wireframeIndicesBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.wireframeIndices.size() * sizeof(unsigned int), mesh.wireframeIndices.data(), GL_DYNAMIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.wireframeIndices.size() * sizeof(uint32_t), mesh.wireframeIndices.data(), GL_DYNAMIC_DRAW);
 
 
 	// Unbind the VAO, VBO, AND INDEX BUFFER
