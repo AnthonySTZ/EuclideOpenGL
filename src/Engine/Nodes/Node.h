006d843f3b45c4fac7e9a4f538e96797b0aca3d0
AnthonySTZ <anthonycandy2309@gmail.com>
2025-05-13 22:18:41 +0200

    add base Node variables

--- src/Engine/Nodes/Node.h
+++ src/Engine/Nodes/Node.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <map>
+#include <string>
+
 class Node {
 
+public:
 
+	Node(std::string name) : nodeName{ name } {};
+	Node() : nodeName{ "Node" } {};
+
+private:
+	std::string nodeName;
+
+	uint32_t inputsNb = 0;
+	uint32_t outputsNb = 0;
+
+	std::map<uint32_t, Node> inputs;
+	std::map<uint32_t, Node> outputs;
 
 };
