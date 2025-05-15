3fe61488fb7e1359ac4b88de56fc6c4574ff404a
AnthonySTZ <anthonycandy2309@gmail.com>
2025-05-16 00:48:03 +0200

    add transform parameters

--- src/Engine/Core/Nodes/Transform.h
+++ src/Engine/Core/Nodes/Transform.h
@@ -2,12 +2,22 @@
 
 #include "Node.h"
 
+#include "Fields/Float3Field.h"
+
 class Transform : public Node {
 
 public:
 	Transform() : Node("Transform") {
 		inputsNb = 1;
 		outputsNb = 1;
+
+		parameters["Translate"] = std::make_unique<Float3Field>("Translate");
+		parameters["Scale"] = std::make_unique<Float3Field>("Scale");
+		parameters["Rotate"] = std::make_unique<Float3Field>("Rotate");
+
+		paramOrder.push_back("Translate");
+		paramOrder.push_back("Scale");
+		paramOrder.push_back("Rotate");
 	};
 
 	Mesh processOutput(uint32_t index) override;
