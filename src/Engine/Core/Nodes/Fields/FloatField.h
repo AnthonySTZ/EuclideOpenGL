be558210ecb5ca5e55b43398ea59ad552f2182e2
AnthonySTZ <anthonycandy2309@gmail.com>
2025-05-19 10:46:12 +0200

    add clamp min max to float field

--- src/Engine/Core/Nodes/Fields/FloatField.h
+++ src/Engine/Core/Nodes/Fields/FloatField.h
@@ -7,7 +7,8 @@
 class FloatField : public Field {
 
 public:
-	FloatField(const char* name, float defaultValue = 0.0f) : label{std::string(name)}, value{defaultValue} {}
+	FloatField(const char* name, float defaultValue = 0.0f, float min=-FLOAT_MAXI, float max=FLOAT_MAXI)
+		: label{ std::string(name) }, value{ defaultValue }, min{min}, max { max } {}
 
 	void draw() override;
 	float getValue() const { return value; }
@@ -15,5 +16,7 @@ public:
 private:
 	std::string label;
 	float value;
+	float min = -FLOAT_MAXI;
+	float max = FLOAT_MAXI;
 
 };
