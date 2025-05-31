7ac41dec92b56c23badf6fd78e7b25ede3dfdcad
AnthonySTZ <anthonycandy2309@gmail.com>
2025-05-31 11:00:40 +0200

    create fileDialog file

---
+++ src/Engine/Core/CustomImGui/FileDialog.cpp
@@ -0,0 +1,15 @@
+#include "FileDialog.h"
+
+std::string FileDialog::openDialog(std::string name, std::string label, std::string type, std::string path)
+{
+    /*
+        Open a file explorer to choose a file.
+        name : ImGui key
+        label : Top name of the dialog
+        type : type a choosing files, separated by comma : e.g ".cpp,.txt,.h"
+        path : starting path folder    
+    */
+
+
+    return std::string();
+}
--- src/Engine/Core/CustomImGui/FileDialog.h
+++ src/Engine/Core/CustomImGui/FileDialog.h
@@ -1,2 +1,11 @@
 #pragma once
 
+#include <imgui.h>
+#include <string>
+
+
+class FileDialog {
+
+	static std::string openDialog(std::string name, std::string label, std::string type, std::string path);
+
+};
