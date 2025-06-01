dfbd69c31cf8fb4028f07a037147595941200ed7
AnthonySTZ <anthonycandy2309@gmail.com>
2025-06-01 13:48:41 +0200

    loadTextureFromFile

--- src/Engine/Core/CustomImGui/ImGuiUtils.h
+++ src/Engine/Core/CustomImGui/ImGuiUtils.h
@@ -5,6 +5,9 @@
 #include <imgui_impl_opengl3.h>
 #include <string>
 #include <iostream>
+#include <glad/glad.h>
+
+#include "stb_image.h"
 
 inline ImVec2 operator+(const ImVec2& lvec, const ImVec2& rvec) {
 	return ImVec2(lvec.x + rvec.x, lvec.y + rvec.y);
@@ -42,4 +45,24 @@ static int StringImGuiCallBack(ImGuiInputTextCallbackData* data) {
 
 static void printImVec2(const char* name, ImVec2& vec){
 	std::cout << name << " | X: " << vec.x << " | Y: " << vec.y << "\n";
+}
+
+static GLuint loadTextureFromFile(const char* filename, int* out_width, int* out_height) {
+    int channels;
+    unsigned char* data = stbi_load(filename, out_width, out_height, &channels, 4); // force RGBA
+    if (data == NULL)
+        return 0;
+
+    GLuint texture_id;
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // good for icons
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *out_width, *out_height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+    stbi_image_free(data);
+    return texture_id;
 }
