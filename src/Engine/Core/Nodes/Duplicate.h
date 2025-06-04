da7501712bc2b0553015de334758710a726f40d2
AnthonySTZ <anthonycandy2309@gmail.com>
2025-06-04 14:27:46 +0200

    set default copies of 2 for duplicate

--- src/Engine/Core/Nodes/Duplicate.h
+++ src/Engine/Core/Nodes/Duplicate.h
@@ -12,7 +12,7 @@ public:
 		inputsNb = 1;
 		outputsNb = 1;
 
-		parameters["nCopies"] = std::make_unique<IntField>("Nb of Copies", 1, 0);
+		parameters["nCopies"] = std::make_unique<IntField>("Nb of Copies", 2, 0);
 		parameters["Translate"] = std::make_unique<Float3Field>("Translate");
 		parameters["Scale"] = std::make_unique<Float3Field>("Scale", std::array<float, 3>{1.0f, 1.0f, 1.0f});
 		parameters["Rotate"] = std::make_unique<Float3Field>("Rotate");
